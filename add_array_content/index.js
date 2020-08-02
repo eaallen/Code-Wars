672e2a3f874a77bcbeb3e39f65ef8a3c26ede3aa
eaallen <elijah.a.allen@gmail.com>
2020-08-02 10:50:34 -0600

    done

--- add_array_content/index.js
+++ add_array_content/index.js
@@ -1,6 +1,6 @@
 /********************************************************************************************
- * My answer to adding the contents of an array. Notice I made this more scalabale so you can 
- * preform actions on as many arrays as you wish and you can preform 4 basic math opperations
+ * My answer to adding the contents of an array. Notice I made this more scalable  so you can 
+ * preform actions on as many arrays as you wish and you can preform 4 basic math operations
  * 
  ********************************************************************************************/
 
@@ -9,10 +9,11 @@ const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const divide = (a, b) => a / b
 function math_array_content(arr, method_per_arr, method_for_whole){ // expecting an array of arrays 
-    return arr.map(el=> el = el.reduce(method_per_arr)).reduce(method_for_whole || method_per_arr)
+    return arr.map(el=> el.reduce(method_per_arr)).reduce(method_for_whole || method_per_arr)
 }
 
 let arr = math_array_content([
     [1,2,3],
     [4,5,6],
 ], divide,add)
+console.log(arr)
