e7057b7b223934399f706507972ea9c0429dca18
eaallen <elijah.a.allen@gmail.com>
2020-08-03 08:57:07 -0600

    yup

---
+++ invert_bool_to_number/index.js
@@ -0,0 +1,11 @@
+/***********************************************************
+ * true => 0 false =>1
+ **********************************************************/
+
+function invert_bool(bool){
+    if(bool){
+        return 0 
+    }
+    return 1
+}
+console.log(invert_bool(false))
