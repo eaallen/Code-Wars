548303ecd49ae02ac070f4d8ad466246af80ef74
eaallen <elijah.a.allen@gmail.com>
2020-08-04 00:01:48 -0600

    yup

---
+++ phone_number/index.js
@@ -0,0 +1,12 @@
+/***********************************************************
+ * turn an array of 10 numbers into a phone number string
+ **********************************************************/
+
+function createPhoneNumber(numbers){
+    let str = numbers.join('').toString()
+    let match_arr = str.match(/^(\d{3})(\d{3})(\d{4})$/)
+    return `(${match_arr[1]}) ${match_arr[2]}-${match_arr[3]}`
+}
+
+let phone = createPhoneNumber([1,2,3,4,5,6,7,8,9,0])
+console.log(phone)
