509710f99c9a50c730f6a8efcd9e8cd47c79209b
eaallen <elijah.a.allen@gmail.com>
2020-08-07 08:58:21 -0600

    curses

---
+++ simple_max_digit_sum/index.js
@@ -0,0 +1,15 @@
+function solve(n){
+    let max_arr = Array.from(Array(n+1), (x,i) => (i).toString().split('').map(item=>{return parseInt(item)}).reduce((a,b)=>a+b))
+    let num_arr = Array.from(Array(n+1), (x,i) => i)
+    let index = []
+    max_arr.map((item,idx)=>{
+        if(item===Math.max(...max_arr)){
+            index.unshift(idx)
+        }
+    })
+    return num_arr.indexOf(index[0])  
+}
+
+console.log(solve(48))
+
+console.log(Array(48));
