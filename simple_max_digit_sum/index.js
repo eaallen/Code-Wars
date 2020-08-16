6f65729148363afe6b9253b4bc896a70ac7121e3
eaallen <elijah.a.allen@gmail.com>
2020-08-16 09:02:12 -0600

    gurr

--- simple_max_digit_sum/index.js
+++ simple_max_digit_sum/index.js
@@ -2,14 +2,15 @@ function solve(n){
     let max_arr = Array.from(Array(n+1), (x,i) => (i).toString().split('').map(item=>{return parseInt(item)}).reduce((a,b)=>a+b))
     let num_arr = Array.from(Array(n+1), (x,i) => i)
     let index = []
-    max_arr.map((item,idx)=>{
-        if(item===Math.max(...max_arr)){
-            index.unshift(idx)
-        }
-    })
-    return num_arr.indexOf(index[0])  
+    // max_arr.map((item,idx)=>{
+    //     if(item===Math.max(...max_arr)){
+    //         index.unshift(idx)
+    //     }
+    // })
+    console.log(Math.max(...max_arr.reverse()))
+    return max_arr.indexOf(Math.max(...max_arr))
 }
 
 console.log(solve(48))
 
-console.log(Array.from(Array(480+1), (x,i) => i));
+console.log(Array.from(Array(4+1), (x,i) => i));
