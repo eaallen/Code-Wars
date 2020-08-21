7a5bea6bcf9d45d148de45ae6f08c36c71e5bb6f
eaallen <elijah.a.allen@gmail.com>
2020-08-21 08:38:24 -0600

    bot will now take you places

--- chatbot/index.js
+++ chatbot/index.js
@@ -12,5 +12,36 @@ function fire(str){
 function handle_data(resp){
     console.log(resp)
     let intent = resp.data.intents[0].name
+    let entities = resp.data.entities
     document.getElementById('output').innerHTML = intent + " : " + resp.data.intents[0].confidence
+    console.log("DATA----->",show_bot_action(intent, {...entities}))
+}
+
+function show_bot_action(intent, obj){
+    const intents = {
+        IN_CHANGE_PAGE: change_page,
+        IN_ELIJAHALLEN_GET_WEATHER:"handle intent only",
+        IN_ELIJAHALLEN_GREETING: read_entity,
+    }
+    if(typeof intents[intent] === "function"){
+       return intents[intent](obj)
+    }
+    return intents[intent]
+    
+    
+}
+
+function read_entity(obj){
+    console.log(obj)
+}
+
+function change_page(entity){
+    let cmd = entity['page_of_website:change_page'][0].value
+    const links = {
+        home: "https://codewarsdemos.firebaseapp.com/",
+        about:"https://codewarsdemos.firebaseapp.com/Code-Wars/about/",
+        index:"./"
+    }
+    window.location.href = links[cmd]
+    return links[cmd]
 }
