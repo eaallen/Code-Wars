19d61325c9ce6c59ac8ef2b06786365ebba2aff7
eaallen <elijah.a.allen@gmail.com>
2020-08-20 22:24:18 -0600

    progress

--- chatbot/index.js
+++ chatbot/index.js
@@ -1,4 +1,16 @@
+function fire(str){
+    axios.get('https://api.wit.ai/message',{
+        headers:{'Authorization': 'Bearer NDE5TQ3JWRA327RQF2USO4TIQ7SW234B'},
+        params:{
+            'v': '20200819',
+            'q': str,
+        }
+    }).then(handle_data).catch(err=>document.getElementById('output').innerHTML = err)
+    // document.getElementById('output').innerHTML = ""
 
-function init(){
-    
+}
+function handle_data(resp){
+    console.log(resp)
+    let intent = resp.data.intents[0].name
+    document.getElementById('output').innerHTML = intent + " : " + resp.data.intents[0].confidence
 }
