a88b2fe8e3da3e8218dd53465924b761bd420427
eaallen <elijah.a.allen@gmail.com>
2020-11-07 21:18:31 -0700

    success

--- piglatin/index.js
+++ piglatin/index.js
@@ -1,11 +1,24 @@
 function pigIt(str) {
+    str = str.toString()
     const pig = str.split(' ').map(x => {
-        x = x.split('')
-        const letter = x.shift()
-        x.push(letter + "ay")
-        return x.join('')
+        console.log(x)
+        if (x.charCodeAt(0) >= 65) {
+            x = x.split('')
+            if (x.length < 2) {
+                return x.join('') + 'ay'
+            }
+            const letter = x.shift()
+            // use ascii to determin what the char is
+            x[x.length - 1].charCodeAt(0) >= 65 ? x.push(letter + "ay") : x.splice((x.length - 2), 0, (letter + 'ay'))
+            return x.join('')
+        } else {
+            return x
+        }
     })
     return pig.join(' ')
-
 }
-console.log(pigIt('eli awesome face.'))
+
+
+
+console.log(pigIt('@el>i awesome@ face !'))
+console.log('a'.charCodeAt(0))
