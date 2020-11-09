2fe96dbfb696c732fc526cbe8d34774b284ad678
eaallen <elijah.a.allen@gmail.com>
2020-11-09 07:59:58 -0700

    got em

--- py/Tribonacci.py
+++ py/Tribonacci.py
@@ -1,7 +1,11 @@
 def tribonacci(signature, n):
-    for x in range(n):
-        sum_of = sum(signature,len(signature)-3)
-        print (sum_of)
+    if n == 0:
+        return []
+    original_len = len(signature)
+    if n < original_len :
+        return signature[0:n]
+    for x in range(n-original_len):
+        sum_of = sum(signature[len(signature)-3:len(signature)])
         signature.append(sum_of)
     return signature
-print(tribonacci([0,0,1],6))
+print(tribonacci([194, 117, 173],489))
