fb94fb4bac8df42c6a2f0d43e16d68d65563931c
eaallen <elijah.a.allen@gmail.com>
2021-01-13 09:20:44 -0700

    format comments

--- IS_542/hw2/index.js
+++ IS_542/hw2/index.js
@@ -1,22 +1,13 @@
-/*
-Compute the ith Fibonacci number (1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …)
-Compute n! (factorial) for integer n ≥ 0
-Compute the sum of all integers between two given integers (inclusive)
-Given a number of cents, print to the browser console the corresponding U.S. 
+
+class Homework2 {
+    /*
+    Given a number of cents, print to the browser console the corresponding U.S. 
     coins that total to the given number.  Print the solution that needs the fewest coins. 
     Only use pennies, nickels, dimes, and quarters.  
     Example: for 113, the answer is “4 quarters”, “1 dime”, “3 pennies”. 
     Do not print the case where the solution calls for 0 of the coin (e.g. don’t print “0 nickels”). 
     Use the singular word if the value is 1, or the plural if the coin count is greater than 1.
-Feeling adventurous?  
-    Hook up your code to a web interface that lets the user specify the inputs 
-    and invoke the various methods at the click of a button.
-
-Turn in on Learning Suite by submitting a PDF document of your web page that includes the 
-JavaScript code for these four tasks.
-*/
-class Homework2 {
-
+    */
     centConverter = cents => {
         const quarter = Math.floor(cents / 25)
         const dime = Math.floor((cents - (quarter * 25)) / 10)
@@ -35,6 +26,7 @@ class Homework2 {
         return str
     }
 
+    // Compute the ith Fibonacci number (1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …)
     fibonacci = number => {
         let num = 1
         const arr = [0, 1]
@@ -45,12 +37,14 @@ class Homework2 {
         return num
     }
 
+    // Compute n! (factorial) for integer n ≥ 0
     factorial = number => {
         let sum = number
         for (let i = number - 1; i > 0; i--) sum *= i
         return sum
     }
 
+    // Compute the sum of all integers between two given integers (inclusive)
     sumBetweenInts = (a, b) => {
         let start = a <= b ? a : b // get the lowest number to start
         let end = start === a ? b : a
@@ -64,15 +58,15 @@ class Homework2 {
 }
 
 const hw2 = new Homework2()
-// console.log(hw2.factorial(4))
-// console.log(hw2.fibonacci(10))
+console.log(hw2.factorial(4))
+console.log(hw2.fibonacci(10))
 console.log(hw2.centConverter(2))
 
 
 class DOMBuilder {
     createFunctionUI = (func, params) => {
-       const form = document.createElement('form')
-       form.setAttribute('id',params.id+'_form')
-       
+        const form = document.createElement('form')
+        form.setAttribute('id', params.id + '_form')
+
     }
 }
