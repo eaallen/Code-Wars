348abb1b752de22b92a2ff55d744e4a9a555988c
eaallen <elijah.a.allen@gmail.com>
2021-02-04 20:50:58 -0700

    css

--- IS_542/hw5_project_1/js/scriptures.js
+++ IS_542/hw5_project_1/js/scriptures.js
@@ -12,9 +12,9 @@ const Scriptures = (function () {
     const CLASS_BOOKS = 'books'
     const CLASS_VOLUMES = 'volume'
     const DIV_SCRIPTURE_NAVIGATOR = 'scripnav'
-    const DIV_SCRIPTURES ='scriptures'
+    const DIV_SCRIPTURES = 'scriptures'
     const TAG_HEADERS = 'h5'
-    
+
     const REQUEST_GET = "GET"
     const REQUEST_STATUS_OK = 200
     const REQUEST_STATUS_ERROR = 400
@@ -23,46 +23,46 @@ const Scriptures = (function () {
     const URL_BOOKS = `${URL_BASE}mapscrip/model/books.php`
     const URL_VOLUMES = `${URL_BASE}mapscrip/model/volumes.php`
     //--------------------------------------- Variables -----------------------------------
-    
+
     // useing state to keep track of the state of these varaibles in this module
     const state = {
-        books:null, // array
-        volumes:null // array
+        books: null, // array
+        volumes: null // array
     }
-    
+
     // ----------------------------------- Functions --------------------------------------
     const HTML = HTMLHelper // statuc class with helper functions 
 
-    const onHashChange = function(){
+    const onHashChange = function () {
         console.log('on hash change')
         const id_array = []
         const location = window.location
         const hash = location.hash
 
-        if(hash !== "" && hash.length > 1){
+        if (hash !== "" && hash.length > 1) {
             id_array.push(...hash.slice(1).split(":"))
         }
 
-        if(hash.length <= 0){
+        if (hash.length <= 0) {
             navigateHome()
-        }else if(id_array.length === 1){
+        } else if (id_array.length === 1) {
             let volume_id = Number(id_array[0]) // convert id_array[0] from str to num
 
-            if(volume_id < state.volumes[0].id || volume_id > state.volumes.slice(-1)[0].id){
+            if (volume_id < state.volumes[0].id || volume_id > state.volumes.slice(-1)[0].id) {
                 navigateHome()
-            }else{
+            } else {
                 navigateHome(volume_id)
             }
-        } else{
+        } else {
             let book_id = Number(id_array[1])
-            console.log('----',state.books[book_id])
+            console.log('----', state.books[book_id])
             if (state.books[book_id] === undefined) {
                 navigateHome()
-            }else {
+            } else {
                 if (id_array.length === 2) {
-                    
-                }else{
-                let chapter = Number(id_array[2])
+
+                } else {
+                    let chapter = Number(id_array[2])
                     // video part 6 -- 6:53 time in
                     navigateChapter(book_id, chapter)
                 }
@@ -70,21 +70,39 @@ const Scriptures = (function () {
             }
         }
 
-        
+
     }
 
-    const navigateBook = function(book_id){
+    const navigateBook = function (book_id) {
         console.log('book', book_id)
     }
-    const navigateChapter = function(book_id, chapter_id){
-        console.log('book', book_id, 'chapter', chapter_id)
+    const navigateChapter = function (book_id, chapter_id) {
+        if (bookChapterValid(book_id, chapter_id)) {
+           console.log('book', book_id, 'chapter', chapter_id)
+           
+        }else{
+            console.error('Book or Chapter not Valid!')
+        }
     }
 
-    const bookChapterValid = function(){
-        let book = state.books[id]
+    // refactor this for better validation 
+    const bookChapterValid = function (book_id, chapter) {
+        let book = state.books[book_id]
+
+        // bad book, bad chapter, or chapter number is not in book
+        if (book === undefined || chapter < 0 || chapter > book.numChapters) {
+            return false
+        }
+
+        // chapter is zero and book has 1 or more chapters 
+        if (chapter === 0 && book.numChapters > 0) {
+            return false
+        }
+
+        return true
     }
 
-    const navigateHome = function(volume_id){
+    const navigateHome = function (volume_id) {
         const divs = [
             '<div>Old Testament </div>',
             '<div>New Testament</div>',
@@ -92,8 +110,8 @@ const Scriptures = (function () {
             '<div>Doctrine and Covenants</div>',
             '<div>Pearl of Great Price</div>',
         ]
-        document.getElementById(DIV_SCRIPTURES).innerHTML = divs.join('')+volume_id
-        
+
+        document.getElementById(DIV_SCRIPTURES).innerHTML = divs.join('') + (volume_id ? volume_id : '')
     }
 
     const cacheBooks = function (callback) {
@@ -109,6 +127,8 @@ const Scriptures = (function () {
         });
         if (typeof callback === 'function') {
             callback(state.volumes)
+            // show the home options 
+            navigateHome()
         }
     }
 
