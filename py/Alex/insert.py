52740690e194a4d64e71b69e577b4fb4d9b0a2c5
eaallen <elijah.a.allen@gmail.com>
2021-09-10 15:16:47 -0600

    sql

--- py/Alex/insert.py
+++ py/Alex/insert.py
@@ -3,8 +3,8 @@ import sqlite3
 def insertHighScore(name, score, date):
   con = sqlite3.connect('highScores.db')
   cur = con.cursor()
-  string = f'INSERT INTO scores VALUES (\'{name}\', {score},\'{date}\')'
-  cur.execute(string)
+  sql = f'INSERT INTO scores VALUES (\'{name}\', {score},\'{date}\')'
+  cur.execute(sql)
   con.commit()
   con.close()
   return 
